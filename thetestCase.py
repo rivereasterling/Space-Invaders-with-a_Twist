c68bb91b258cbe271a3d1cca9891aaa95de73110
rivereasterling <82299321+rivereasterling@users.noreply.github.com>
2021-04-14 23:41:35 -0400

    Update thetestCase.py

--- thetestCase.py
+++ thetestCase.py
@@ -10,15 +10,15 @@ class All_Test(unittest.TestCase):
 	#the above test wouldn't work for me so I know that somethings up with how pycharm does unit tests in pygame
 	# I tried doing unit test examples outside of pygame and they worked fine so I am completely stumped the above works when put outside pygame but won't even run a test when its in here.
 	def Laser_test(self):
-		self.assertTrue(Laser, True)
+		self.assertTrue(Laser, "there are no lasers")
 	def Ship_test(self):
-			self.assertTrue(Ship,True)
+			self.assertTrue(Ship, "There are no ships")
 	def Enemy_test(self):
-			self.assertTrue(Enemy,True)
+			self.assertTrue(Enemy, "There are no enemies on screen")
 	def Player_test(self):
-			self.assertTrue(Player,True)
+			self.assertTrue(Player, "The player isn't on screen")
 	def Collide_Test(self):
-			self.assertTrue(collide(obj1=Player,obj2= Enemy),True)
+			self.assertTrue(collide(obj1=Player,obj2= Enemy), "The Player can not collide with an enemie")
 
 
 if __name__ == '__main__':unittest.main()
